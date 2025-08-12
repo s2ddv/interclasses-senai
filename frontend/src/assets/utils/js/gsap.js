f7f9bcd916ce6b3b392cf7931935cc4de20960d8
s2ddv <1speedftn@gmail.com>
2025-08-12 14:37:49 -0300

    feat: add the header page,

--- frontend/src/assets/utils/js/gsap.js
+++ frontend/src/assets/utils/js/gsap.js
@@ -1,16 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const splitTypes = document.querySelectorAll('.tituloGSAP')
+const splitTypes = document.querySelectorAll('.textGSAP')
 
 splitTypes.forEach((char, i) => {
 
-    const text = new SplitType(char, { types: 'chars,words'})
+    const text = new SplitType(char, { types: 'chars, words'})
 
     gsap.from(text.chars, { 
         scrollTrigger: { 
             trigger: char,
             start: 'top 80%',
-            end: 'top 20%',
+            end: 'top 40%',
             scrub: true, 
             markers: false
         },
@@ -19,7 +19,32 @@ splitTypes.forEach((char, i) => {
     })
 })
 
-const splitTypes2 = document.querySelectorAll('.textGSAP')
+const splitTypes2 = document.querySelectorAll('.tituloGSAP')
+
+splitTypes2.forEach((char, i) => {
+    
+    const text = new SplitType(char, { types: 'chars, words' })
+
+    gsap.fromTo(text.chars, 
+        { color: 'white'  },
+        {
+            color: 'red',
+            duration: 0.6,
+            stagger: 0.1,
+            scrollTrigger: { 
+                trigger: char,
+                start: 'top 80%',
+                end: 'top 20%',
+                scrub: true,
+                markers: false
+            }
+        }
+        
+    )
+})
+
+/*
+const splitTypes2 = document.querySelectorAll('.GSAP')
 
 splitTypes2.forEach((char, i) => { 
 
@@ -37,6 +62,7 @@ splitTypes2.forEach((char, i) => {
         y: -20,
         transformOrigin: 'top',
         stagger: 0.1,
-        duration: 1
+        duration: 0.6
     })
 })
+*/
--- frontend/src/assets/utils/js/script.js
+++ frontend/src/assets/utils/js/script.js
@@ -2,5 +2,9 @@ const menuButton = document.getElementById("menu");
 const sidebar = document.getElementById("sidebar");
 
 menuButton.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+menuButton.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
